1f5a6ad692b79f4c70e26db0d9e24782472ec264
Ashley Whitehouse <ashley.whitehouse@dvsa.gov.uk>
2021-03-11 18:12:31 +0000

    first basic version of the page crawler. Still needs uniquiness with files and speeding up

--- src/main/java/ScanPage.java
+++ src/main/java/ScanPage.java
@@ -33,46 +33,61 @@ public class ScanPage {
 
     private static final Logger LOGGER = LogManager.getLogger(ScanPage.class);
     Set<String> urlSet = new HashSet<>();
+    Set<String> allUniqueUrls = new HashSet<>();
 
     public void scanForSubTree(String baseURL) throws IOException, IllegalBrowserException {
         Browser.navigate().get(baseURL);
         List<WebElement> links = Browser.navigate().findElements(By.tagName("a"));
         for (WebElement link : links) {
+
             setUrl(link.getAttribute("href"));
-            if (!url.contains(getBaseURL()) || (url.contains("logout")) || (url.contains("?sort"))) {
-                LOGGER.info(getUrl().concat(" did not get scanned"));
-            } else {
-                urlSet.add(getUrl());
+            if (!(url == null)) {
+
+                if (!url.contains(getBaseURL()) || (url.contains("logout")) || (url.contains("?sort")) || url.contains("?page") || url.contains(".rtf")) {
+                    LOGGER.info(getUrl().concat(" did not get scanned"));
+                } else {
+                    urlSet.add(getUrl());
+                }
+//TODO: return the set found in this function. Makes it easier for end user to manipulate afterwards.
             }
         }
     }
 
     public Set<String> scanForAllURLs(String internalUrl) throws IOException, IllegalBrowserException {
-        Set<String> allUrls = new HashSet<>();
+//        Set<String> allUrls = new HashSet<>();
+        allUniqueUrls.add(internalUrl);
 
         scanForSubTree(internalUrl);
 
-        String urlRegex = null;
-
-        boolean stillAdding = true;
+        String urlRegex;
         int previousAllUrlsSize = 0;
-        while (stillAdding) {
+
+        while (true) {
             for (String url : urlSet) {
-                urlRegex = url.replaceAll("\\/[0-9]{1,45}\\/", "\\/[0-9]{1,45}\\/");
-                if (!allUrls.contains(urlRegex)) {
-                    allUrls.add(url);
+                urlRegex = url.replaceAll("\\/[0-9]{1,45}\\/", "/[0-9]{1,45}/");
+                String finalUrlRegex = urlRegex;
+                boolean containsUrl = allUniqueUrls.stream().anyMatch(x -> x.matches(finalUrlRegex));
+
+                if (!containsUrl) {
+                    allUniqueUrls.add(url);
                 }
             }
-            if (allUrls.size() == previousAllUrlsSize) {
-                stillAdding = false;
+            System.out.println(urlSet);
+            System.out.println(allUniqueUrls);
+            if (allUniqueUrls.size() == previousAllUrlsSize) {
+                break;
             }
-            previousAllUrlsSize = allUrls.size();
+            previousAllUrlsSize = allUniqueUrls.size();
 
-            for (String url : allUrls) {
+            urlSet.clear();
+            for (String url : allUniqueUrls) {
                 scanForSubTree(url);
             }
         }
 
-        return allUrls;
+        //TODO: OPTIMIZE TO ONLY SCANNED PAGES NOT YET SCANNED> FIND OUT HOW THEY ARE ADDED TO THE SET AND EXCLUDE A SET COUNT EACH TIME.
+        //TODO: COME UP WITH WAY TO EXCLUDE URLS WITH FILE/0432894324
+
+        return allUniqueUrls;
     }
 }
