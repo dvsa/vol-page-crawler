b1379e21e3d24109a429b2ac72a6c5d1f12430a3
Ashley Whitehouse <ashley.whitehouse@dvsa.gov.uk>
2021-03-22 11:25:26 +0000

    relatively stable run of the page crawler with pagination looping in place. Still using try catches because of how unstable selenium is. Not sure if there is a way around this yet.

--- src/main/java/ScanPage.java
+++ src/main/java/ScanPage.java
@@ -1,15 +1,17 @@
+import Util.Waits;
 import activesupport.IllegalBrowserException;
 import activesupport.driver.Browser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
+import static java.lang.Thread.sleep;
 
 
 public class ScanPage {
@@ -35,58 +37,111 @@ public class ScanPage {
     Set<String> urlSet = new HashSet<>();
     Set<String> allUniqueUrls = new HashSet<>();
 
-    public void scanForSubTree(String baseURL) throws IOException, IllegalBrowserException {
-        Browser.navigate().get(baseURL);
+
+    public void scanForSubTreeLoopingPagination() throws IOException, IllegalBrowserException, InterruptedException {
+
+        boolean paginationPresent = Browser.navigate().findElements(By.xpath("//a[@rel='Next']")).size() > 0;
+
+        if (paginationPresent) {
+            Browser.navigate().findElement(By.xpath("//a[contains(@href,'limit=50') and text()='50']")).click();
+            Waits.waitForTableToLoad50Rows(Browser.navigate());
+            try {
+                scanForSubTree();
+            } catch (StaleElementReferenceException e) {
+                sleep(5000);
+                scanForSubTree();
+            }
+            paginationPresent = Browser.navigate().findElements(By.xpath("//a[@rel='Next']")).size() > 0;
+
+            int pageCount = 1;
+            while(paginationPresent && pageCount <= 10){
+                Browser.navigate().findElement(By.xpath("//a[@rel='Next']")).click();
+                try {
+                    Waits.waitForElementToBePresent(Browser.navigate(), String.format("//a[text()='%s' and contains(@href,'page=')]", pageCount));
+                } catch (NoSuchElementException e) {
+                    Browser.navigate().findElement(By.xpath("//a[@rel='Next']")).click();
+                }
+                pageCount++;
+                try {
+                    scanForSubTree();
+                } catch (StaleElementReferenceException e) {
+                    sleep(5000);
+                    scanForSubTree();
+                }
+                paginationPresent = Browser.navigate().findElements(By.xpath("//a[@rel='Next']")).size() > 0;
+            }
+        }
+        else {
+            scanForSubTree();
+        }
+
+    }
+
+    public void scanForSubTree() throws IOException, IllegalBrowserException {
+
         List<WebElement> links = Browser.navigate().findElements(By.tagName("a"));
+
+
         for (WebElement link : links) {
 
             setUrl(link.getAttribute("href"));
             if (!(url == null)) {
+                boolean isFileDownloadLink = url.matches(".*file/[0-9]{0,45}");
 
-                if (!url.contains(getBaseURL()) || (url.contains("logout")) || (url.contains("?sort")) || url.contains("?page") || url.contains(".rtf")) {
+                if (!url.contains(getBaseURL())
+                        || (url.contains("logout"))
+                        || (url.contains("?sort"))
+                        || url.contains("?page")
+                        || isFileDownloadLink
+                        || url.contains("?startDate")
+                        || url.contains("?validForYear")
+                        || url.contains("postScoringReport")
+                        || url.contains("alignStock")
+                        || url.contains("ms-word:ofe")) {
                     LOGGER.info(getUrl().concat(" did not get scanned"));
                 } else {
                     urlSet.add(getUrl());
                 }
-//TODO: return the set found in this function. Makes it easier for end user to manipulate afterwards.
             }
         }
     }
 
-    public Set<String> scanForAllURLs(String internalUrl) throws IOException, IllegalBrowserException {
-//        Set<String> allUrls = new HashSet<>();
+    public Set<String> scanForAllURLs(String internalUrl) throws IOException, IllegalBrowserException, InterruptedException {
+        //TODO: All ability to enter set of urls.
         allUniqueUrls.add(internalUrl);
 
-        scanForSubTree(internalUrl);
+        Browser.navigate().get(internalUrl);
+        scanForSubTreeLoopingPagination();
 
         String urlRegex;
         int previousAllUrlsSize = 0;
+        Set<String> newUrls = new HashSet<>();
 
         while (true) {
+
+            newUrls.clear();
+
             for (String url : urlSet) {
                 urlRegex = url.replaceAll("\\/[0-9]{1,45}\\/", "/[0-9]{1,45}/");
                 String finalUrlRegex = urlRegex;
                 boolean containsUrl = allUniqueUrls.stream().anyMatch(x -> x.matches(finalUrlRegex));
-
                 if (!containsUrl) {
                     allUniqueUrls.add(url);
+                    newUrls.add(url);
                 }
             }
-            System.out.println(urlSet);
-            System.out.println(allUniqueUrls);
+
             if (allUniqueUrls.size() == previousAllUrlsSize) {
                 break;
             }
             previousAllUrlsSize = allUniqueUrls.size();
 
             urlSet.clear();
-            for (String url : allUniqueUrls) {
-                scanForSubTree(url);
+            for (String url : newUrls) {
+                Browser.navigate().get(url);
+                scanForSubTreeLoopingPagination();
             }
         }
-
-        //TODO: OPTIMIZE TO ONLY SCANNED PAGES NOT YET SCANNED> FIND OUT HOW THEY ARE ADDED TO THE SET AND EXCLUDE A SET COUNT EACH TIME.
-        //TODO: COME UP WITH WAY TO EXCLUDE URLS WITH FILE/0432894324
 
         return allUniqueUrls;
     }
--- src/main/test/java/UrlTest.java
+++ src/main/test/java/UrlTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Set;
 
 public class UrlTest {
@@ -18,16 +20,18 @@ public class UrlTest {
     }
 
     @Test
-    public void internalScan() throws IOException, IllegalBrowserException {
+    public void internalScan() throws IOException, IllegalBrowserException, InterruptedException {
         ScanPage scanPage = new ScanPage();
         Browser.navigate().get(internalUrl);
         Browser.navigate().findElement(By.name("username")).sendKeys("usr291");
         Browser.navigate().findElement(By.name("password")).sendKeys("Get755Cob!VoxPig");
         Browser.navigate().findElement(By.name("submit")).click();
         Set<String> allUrs = scanPage.scanForAllURLs(internalUrl);
+        Object[] allUrlsArray = allUrs.toArray();
+        Arrays.sort(allUrlsArray);
 
-        for (String url : allUrs) {
-            DataHandler.writeToFile("allUrls.txt", url);
+        for (Object url : allUrlsArray) {
+            DataHandler.writeToFile("allUrls.txt", url.toString());
         }
 
 
@@ -36,5 +40,17 @@ public class UrlTest {
 //            DataHandler.writeToFile("tableUrls.txt", scanPage.getUrl());
 //        }
 
+        //TODO: Need to go down different routes for different choices.
+        // Need to look into storing choices for each route?
+        // Maybe have something that goes through an entire journey and keep going until it loops to a previously stored page within that single journey
+        // Proceed to do the same journey with different data.
+        // Look at different choices and what could create a new route and how to store that as a fork to return to. Submit buttons, radio buttons.
+        // Look at where forms go via actions and that stuff.
+
+        // If there are errors, store the pages for assessing and possibly create loop to fill in missing data.
+
+        // Store the url regex and then store elements and answer to that page and loop over those.
+
+        // Add way to pass in list of ids that are expected submit buttons? Or just find all submit buttons?
     }
 }
